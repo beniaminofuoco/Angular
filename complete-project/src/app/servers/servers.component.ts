cc2e010eab805fca065131ae34cb81b6c4e10497
beniaminofuoco <d7393@italiaonline.it>
2018-04-03 17:22:20 +0200

    Binding

--- complete-project/src/app/servers/servers.component.ts
+++ complete-project/src/app/servers/servers.component.ts
@@ -3,15 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // selector: '[app-servers]' può essere richiamato <div app-servers>
   // selector: '.app-servers' può essere richiamato in <div class="app-servers">
-  selector: 'app-servers', // Modalità consigliata
+  selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit() {
 
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus='Server was Created';
   }
 }
