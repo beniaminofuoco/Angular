56908bf8f8bd049859d5b2054054dcc281a72104
beniaminofuoco <d7393@italiaonline.it>
2018-04-03 16:11:00 +0200

    bug

--- complete-project/src/app/server/server.component.ts
+++ complete-project/src/app/server/server.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent{
     serverID: number = 10;
-    serverStatus : string= 'Offline';
+    serverStatus: string= 'Offline';
 
     getServerStatus(){
         return this.serverStatus;
