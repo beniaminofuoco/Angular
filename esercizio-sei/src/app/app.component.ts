36d431f1ea394752f90c6a2f0f282703da886d41
beniaminofuoco <d7393@italiaonline.it>
2018-04-20 17:18:56 +0200

    Esercizio 6 iniziato

--- esercizio-sei/src/app/app.component.ts
+++ esercizio-sei/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') form: NgForm;
+  defaultLevel= 'advanced';
+  submitted = false;
 
+  dati = {
+    email: '',
+    password: '',
+    level: ''
+  };
+
+  onSubmit() {
+    this.submitted = true;
+
+    this.dati.email = this.form.value.email;
+    this.dati.password = this.form.value.password;
+    this.dati.level = this.form.value.level;
+
+    this.form.reset();
+  }
 }
